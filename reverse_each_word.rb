b7ec23ad79d17c3015c7141bc86078d59e2a71be
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-05 22:53:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence1)
     new_array = []
    sentence1.split.each do |word|
      new_array << word.reverse
-    binding.pry
+    #binding.pry
   end
     new_array.join
 end
